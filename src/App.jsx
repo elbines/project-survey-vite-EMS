4890d61990655849d446f8602e8ca789a6f5e064
“mikaeloling” <mikael.oling@gmail.com>
2023-09-27 22:03:09 +0300

    bla bla

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import {MultiStepForm} from "./Components/MultiStepForm/MultiStepForm"
 export const App = () => {
   return <MultiStepForm/>;
 };
-
+console.log("App.jsx");
 /*
 Elba:HowOften,Button
-Mikael:Suggestions,Footer,Hero
+Mikael:Suggestions,Footer,Hero , A small change 
 Sakura: PrimaryReason, Summary,
 */ 
