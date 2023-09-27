0006a60d28ded613c9c893a2252771f3e847689d
elbines <141184135+elbines@users.noreply.github.com>
2023-09-27 23:33:06 +0200

    HowOften

--- src/Components/HowOften/HowOften.jsx
+++ src/Components/HowOften/HowOften.jsx
@@ -1,12 +1,16 @@
-export const HowOften = () => {
-    return <div>HowOften</div>;
-  };
 
+export const HowOften = ({ updateFormData, value }) => {
+  const handleHowOftenChange = (e) => updateFormData("howOften", e.target.value);
 
-//   2. How frequently did you make an order?
-// (Select Dropdown)
-// [Select One]
-// Monthly
-// Every second month
-// Rarely
-// Never
+  return (
+    <form>
+      <select onChange={handleHowOftenChange} value={value}>
+        <option value="">How often do you order - test</option>
+        <option value="Monthly">Monthly</option>
+        <option value="Every-second-month">Every second month</option>
+        <option value="Rarely">Rarely</option>
+        <option value="Never">Never</option>
+      </select>
+    </form>
+  );
+};
