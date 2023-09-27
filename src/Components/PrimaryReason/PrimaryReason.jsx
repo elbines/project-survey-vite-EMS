9fb32bada5a7faa14c42b59c16ae7540f14f7351
Sakura Tanaka <sito6496t@google.com>
2023-09-27 23:42:34 +0200

    some styling to PrimaryReason

--- src/Components/PrimaryReason/PrimaryReason.jsx
+++ src/Components/PrimaryReason/PrimaryReason.jsx
@@ -2,50 +2,76 @@ import style from "./PrimaryReason.module.css";
 import { SecondaryHeading } from "../SecondaryHeading/SecondaryHeading";
 export const PrimaryReason = ({ updateFormData, value }) => {
   const firstQuestion = (e) => updateFormData("primaryReason", e.target.value);
+  console.log(value);
 
   return (
     <>
       <SecondaryHeading question="Please help us understand why you stopped ordering from us. Choose the primary reason:" />
 
-      <input
-        type="radio"
-        id="highprice"
-        name="primaryReason"
-        value="highprice"
-        onChange={firstQuestion}
-      />
-      <label htmlFor="highprice">Prices were too high</label>
+      <div className={style.input_wrapper}>
+        <div className={style.input_field}>
+          <input
+            type="radio"
+            id="highprice"
+            name="primaryReason"
+            value="highprice"
+            onChange={firstQuestion}
+          />
+          <label htmlFor="highprice">Prices were too high</label>
+        </div>
+        <div className={style.input_field}>
+          <input
+            type="radio"
+            id="quality"
+            name="primaryReason"
+            value="quality"
+            onChange={firstQuestion}
+          />
 
-      <input
-        type="radio"
-        id="quality"
-        name="primaryReason"
-        value="quality"
-        onChange={firstQuestion}
-      />
-      <label htmlFor="quality">Quality of products</label>
-
-      <input
-        type="radio"
-        id="shipping"
-        name="primaryReason"
-        value="shipping"
-        onChange={firstQuestion}
-      />
-      <label htmlFor="shipping">Shipping and delivery issues</label>
-
-      <input
-        type="radio"
-        id="betteralternative"
-        name="primaryReason"
-        value="betteralternative"
-        onChange={firstQuestion}
-      />
-      <label htmlFor="betteralternative">Found better alternatives</label>
-
-      <input type="radio" id="other" name="primaryReason" value="other" onChange={firstQuestion} />
-      <label htmlFor="other">Other</label>
-      <input type="text" id="other" name="primaryReason" value="other" />
+          <label htmlFor="quality">Quality of products</label>
+        </div>
+        <div className={style.input_field}>
+          <input
+            type="radio"
+            id="shipping"
+            name="primaryReason"
+            value="shipping"
+            onChange={firstQuestion}
+          />
+          <label htmlFor="shipping">Shipping and delivery issues</label>
+        </div>
+        <div className={style.input_field}>
+          <input
+            type="radio"
+            id="betteralternative"
+            name="primaryReason"
+            value="betteralternative"
+            onChange={firstQuestion}
+          />
+          <label htmlFor="betteralternative">Found better alternatives</label>
+        </div>
+        <div className={`${style.input_field} ${style.input_other}`}>
+          <div>
+            <input
+              type="radio"
+              id="other"git add .
+              
+              name="primaryReason"
+              value="other"
+              onChange={firstQuestion}
+            />
+            <label htmlFor="other">Other</label>
+          </div>
+          <input
+            type="text"
+            id="other"
+            className={style.text_input}
+            name="primaryReason"
+            onChange={firstQuestion}
+            placeholder="Other..."
+          />
+        </div>
+      </div>
     </>
   );
 };
