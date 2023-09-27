644fb0983a91d455ccf0d07ce53174941f4ddc25
Sakura Tanaka <sito6496t@google.com>
2023-09-27 13:00:55 +0200

    primaryReason is updated

--- src/Components/PrimaryReason/PrimaryReason.jsx
+++ src/Components/PrimaryReason/PrimaryReason.jsx
@@ -1,27 +1,44 @@
-export const PrimaryReason = () => {
+export const PrimaryReason = ({ updateFormData, value }) => {
+  const firstQuestion = (e) => updateFormData("primaryReason", e.target.value)
 
-    return <div>
+    return ( <div>
       <h2>Please help us understand why you stopped ordering from us. Choose the primary reason:</h2>
       
-        <input type = "radio" id = "highprice" name = "primaryReason" value="highprice" />
+        <input type = "radio" id = "highprice" name = "primaryReason" value="highprice" onChange={firstQuestion} />
       <label htmlFor="highprice">Prices were too high</label>
       
-        <input type = "radio" id = "quality" name = "primaryReason" value = "quality" />
+        <input type = "radio" id = "quality" name = "primaryReason" value = "quality"  onChange={firstQuestion}/>
       <label htmlFor="quality">Quality of products</label>
       
-        <input type = "radio" id = "shipping" name = "primaryReason" value = "shipping" />
+        <input type = "radio" id = "shipping" name = "primaryReason" value = "shipping"  onChange={firstQuestion}/>
       <label htmlFor="shipping">Shipping and delivery issues</label>
       
-        <input type = "radio" id = "betteralternative" name = "primaryReason" value = "betteralternative" />
+        <input type = "radio" id = "betteralternative" name = "primaryReason" value = "betteralternative"  onChange={firstQuestion}/>
       <label htmlFor="betteralternative">Found better alternatives</label>
 
-        <input type = "radio" id = "other" name = "primaryReason" value = "other" />
+        <input type = "radio" id = "other" name = "primaryReason" value = "other"  onChange={firstQuestion}/>
       <label htmlFor="other">Other</label>
       <input type="text" id = "other"  name="primaryReason" value = "other" />
 
-      </div>;
+      </div>)
   };
 
+  /*
+  export const Album = ({ updateFormData, value }) => {
+  const favoriteAlbum = (e) => updateFormData("favoriteAlbum", e.target.value);
+  return (
+    <div>
+      <label>Favorite Album:</label>
+      <select value={value} onChange={favoriteAlbum}>
+        <option value="">Select an Album</option>
+        <option value="taylor-swift-midnights">Taylor Swift - Midnights</option>
+        <option value="beyonce-renaissance">Beyonce - Renaissance</option>
+        <option value="drake-21-her-loss">Drake & 21 Savage - Her Loss</option>
+        <option value="sza-sos">SZA - SOS</option>
+      </select>
+    </div>
+  );
+};*/
 // 1. Please help us understand why you stopped ordering from us. Choose the primary reason:
 // (Radio Buttons)
 // Prices were too high
@@ -30,41 +47,41 @@ export const PrimaryReason = () => {
 // Found better alternatives
 // Other (Please specify): [Text Input]
 
-function App() {
-  const [topping, setTopping] = useState("Medium")
+// function App() {
+//   const [topping, setTopping] = useState("Medium")
 
-  return (
-    <div className="App">
-      <h3>Select Pizza Size</h3>
+//   return (
+//     <div className="App">
+//       <h3>Select Pizza Size</h3>
 
-      <input
-        type="radio"
-        name="topping"
-        value="Regular"
-        id="regular"
-        checked={topping === "Regular"}
-      />
-      <label htmlFor="regular">Regular</label>
+//       <input
+//         type="radio"
+//         name="topping"
+//         value="Regular"
+//         id="regular"
+//         checked={topping === "Regular"}
+//       />
+//       <label htmlFor="regular">Regular</label>
 
-      <input
-        type="radio"
-        name="topping"
-        value="Medium"
-        id="medium"
-        checked={topping === "Medium"}
-      />
-      <label htmlFor="medium">Medium</label>
+//       <input
+//         type="radio"
+//         name="topping"
+//         value="Medium"
+//         id="medium"
+//         checked={topping === "Medium"}
+//       />
+//       <label htmlFor="medium">Medium</label>
 
-      <input
-        type="radio"
-        name="topping"
-        value="Large"
-        id="large"
-        checked={topping === "Large"}
-      />
-      <label htmlFor="large">Large</label>
-    </div>
-  )
-}
+//       <input
+//         type="radio"
+//         name="topping"
+//         value="Large"
+//         id="large"
+//         checked={topping === "Large"}
+//       />
+//       <label htmlFor="large">Large</label>
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
