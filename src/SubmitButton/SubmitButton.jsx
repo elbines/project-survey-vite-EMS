99b4b00523690c91ee7ad90c5cb93588f782860c
Sakura Tanaka <sito6496t@google.com>
2023-09-28 12:51:54 +0200

    some changes in SubmitButton

--- src/SubmitButton/SubmitButton.jsx
+++ src/SubmitButton/SubmitButton.jsx
@@ -1,8 +1,17 @@
 import style from "./SubmitButton.module.css";
-export const SubmitButton = ({ text, onClickEvent }) => {
+export const SubmitButton = ({ text, onClickEvent, url }) => {
+  console.log(url);
   return (
-    <button className={style.button} onClick={onClickEvent}>
-      {text}
-    </button>
+    <>
+      {url ? (
+        <a href={url} target="_blank" rel="noreferrer">
+          <button className={style.button}>{text}</button>
+        </a>
+      ) : (
+        <button className={style.button} onClick={onClickEvent}>
+          {text}
+        </button>
+      )}
+    </>
   );
 };
