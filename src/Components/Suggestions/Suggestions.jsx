2b857e55a41d05f73d8eec28cca12909e6ebbb36
“mikaeloling” <mikael.oling@gmail.com>
2023-09-28 16:12:36 +0300

    suggestions

--- src/Components/Suggestions/Suggestions.jsx
+++ src/Components/Suggestions/Suggestions.jsx
@@ -1,7 +1,23 @@
+import style from "./Suggestions.module.css";
 
-export const Suggestions = () => {
-    return <div>Suggestions</div>;
-  };
+export const Suggestions = ({ updateFormData }) => {
+  const thirdQuestion = (e) => updateFormData("thirdQuestion", e.target.value);
+  
+  return (
+    <div>
+      <h2>What suggestions do you have to improve your shopping experience?</h2>
+      <div className={style.input_wrapper}>
+        <textarea
+          id="input"
+          className={style.text_input}
+          placeholder="Please type here"
+          rows="3"
+          onChange={thirdQuestion}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
 
 //   3. What suggestions do you have to improve your shopping experience?
 // (Text Input)
