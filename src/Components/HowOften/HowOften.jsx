91a33ef55ea2278ae158e81e3d09cfae389d5fee
elbines <141184135+elbines@users.noreply.github.com>
2023-09-29 01:27:41 +0200

    small changes btn and howoften

--- src/Components/HowOften/HowOften.jsx
+++ src/Components/HowOften/HowOften.jsx
@@ -1,14 +1,14 @@
 import style from "./HowOften.module.css";
-
+import { SecondaryHeading } from "../SecondaryHeading/SecondaryHeading";
 
 export const HowOften = ({ updateFormData, value }) => {
   const handleHowOftenChange = (e) => updateFormData("howOften", e.target.value);
 
   return (
-    <div>
-     <h2>2. Please help us understand why you stopped ordering from us. Choose the primary reason:</h2>
-    
-      <select onChange={handleHowOftenChange} value={value} className={style.input_wrapper} > 
+    <>
+      <SecondaryHeading question="2. How frequently did you make an order? Select an option below:" />
+
+      <select onChange={handleHowOftenChange} value={value} className={style.input_wrapper} >
 
         <option value="">Options: </option>
         <option value="Monthly">Monthly</option>
@@ -16,6 +16,6 @@ export const HowOften = ({ updateFormData, value }) => {
         <option value="Rarely">Rarely</option>
         <option value="Never">Never</option>
       </select>
-    </div>
+    </>
   );
 };
