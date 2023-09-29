60bdebfe1f77f1efe03cd51f65ca0050fd2ece90
Sakura Tanaka <sito6496t@google.com>
2023-09-29 09:11:08 +0200

    error handling

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,27 +1,21 @@
-import previousQuestionBtnIcon from '/assets/arrow_left.svg';
-import nextQuestionBtnIcon from '/assets/arrow_right.svg';
-import style from './Button.module.css'
+import previousQuestionBtnIcon from "/assets/arrow_left.svg";
+import nextQuestionBtnIcon from "/assets/arrow_right.svg";
+import style from "./Button.module.css";
 
 export const Button = ({ prevStep, nextStep, currentStep }) => {
   //<div className={style.QuestionBtnContainer}>
   return (
     <div>
-      {currentStep > 1 && currentStep < 5 &&
+      {currentStep > 1 && currentStep < 5 && (
         <button onClick={prevStep} className={style.previousButton}>
           <img src={previousQuestionBtnIcon} alt="Previous Step" />
         </button>
-      }
-      {currentStep < 4 &&
+      )}
+      {currentStep < 4 && (
         <button onClick={nextStep} className={style.nextButton}>
           <img src={nextQuestionBtnIcon} alt="Next Step" />
         </button>
-      }
-
+      )}
     </div>
   );
 };
-
-
-
-
-
